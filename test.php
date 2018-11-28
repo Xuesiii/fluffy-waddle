93e28d1e091056d085e643455a563f154df79665
Xuesiii <xsnicolexs@gmail.com>
2018-11-28 12:57:46 +0800

    Tidied up the codess

--- test.php
+++ test.php
@@ -1,23 +1,18 @@
 <html>
 <body>
-Hello world
-Testing line 1, extending line 1 to test whether there will be a merge error s
+    Hello world
+    Testing line 1
 
-Testing line 2
+    Testing line 2
 
 
-Testing line 3
-Left. What happens if we click resolve?
+    Testing line 3
 
-final 
-surfaces
-oh oh oh oh oh you're just another guy
 
-Commit 1 - Happy 
-Commit 2 - Birthday
-Commit 3 - to youuuu~~  <?php
-    echo "hello world";
-?>
+    Commit 1  
+    Commit 2-
+    Commit 3
+
 </body>
 
 
